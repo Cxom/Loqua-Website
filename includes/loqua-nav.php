5316ffcbcb1dd4254e9154556235a6a038d130ec
Cxom <cxomtdoh@yahoo.com>
2017-05-22 05:43:12 -0400

    Add jQuery, Bootstrap, and convert and update navbar

--- includes/loqua-nav.php
+++ includes/loqua-nav.php
@@ -1,22 +1,51 @@
-<nav>
-play.punchtree.tk
-<!--<img id="address" src="" alt="73.50.203.231">-->
-<ul>
-    <li><a href="index.php">Home</a></li>
-    <li class="dropdown">
-          <a class="" href="landmarks.php">Landmarks</a>
-          <div class="dropdown-content">
-              <a href="#">Warp List</a>
-              <a href="#">Building Guide</a>
-              <a href="#">NPCs</a>
-          </div>
-    </li>
-    <li><a href="">Interactives</a></li>
-    <li><a href="dynmap.php">Map</a></li>
-    <li><a href="#">Forums</a></li>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <div id="brand-container">
+                <a class="navbar-brand" id="brand" href="./" title="PunchTree!">👊🌴</a>
+                <p id="brand-text">play.punchtree.net</p>
+            </div>
+        </div>
 
-</ul>
-<a id="profile-button" title="View Profile" href="">
-    PROFILE
-</a>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="landmarks.php">Landmarks</a></li>
+                <li><a href="#">Interactives</a></li>
+<!--                <li class="dropdown">-->
+<!--                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>-->
+<!--                    <ul class="dropdown-menu">-->
+<!--                        <li><a href="#">Action</a></li>-->
+<!--                        <li><a href="#">Another action</a></li>-->
+<!--                        <li><a href="#">Something else here</a></li>-->
+<!--                        <li role="separator" class="divider"></li>-->
+<!--                        <li><a href="#">Separated link</a></li>-->
+<!--                        <li role="separator" class="divider"></li>-->
+<!--                        <li><a href="#">One more separated link</a></li>-->
+<!--                    </ul>-->
+<!--                </li>-->
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="dynmap.php">Map</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Separated link</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
 </nav>
--- landmarks.php
+++ landmarks.php
@@ -5,8 +5,12 @@
         <meta charset="utf-8">
         <link rel="stylesheet" href="styles/reset.css">
         <link rel="stylesheet" href="styles/stylesheet.css">
-        <link rel="stylesheet" type="text/css" href="styles/loqua-nav.css"/>
+        <link rel="stylesheet" href="styles/loqua-nav.css">
         <script type="text/javascript" src="includes/rand-background.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+              integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+              integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
         <title>Loqua Landmarks</title>
     </head>
 <body>
@@ -14,5 +18,9 @@
     <main>
         
     </main>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"
+            integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 </html>
